2180cc79eaa64fb024972271b81fa6a80d469f8b
kartik-barman <mrkartik1st@gmail.com>
2024-04-16 19:17:29 +0530

    Love Calculator

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let calculate = document.querySelector("#calculate-btn");
+let percentage = document.querySelector("#percentage");
+let heart = document.querySelector(".heart")
+calculate.addEventListener("click", () => {
+    let random = Math.floor(Math.random() * 100);
+    console.log(`${random}%`);
+    let p = document.createElement("p");
+    percentage.innerHTML = `${random}%`;
+    heart.appendChild(p)
+    let yourName = document.querySelector(".input1").value;
+    let crushName = document.querySelector("#input2").value;
+    let yourNameText = document.querySelector("#your-name");
+    let crushNameText = document.querySelector("#crush-name");
+    let p1 = document.createElement("p");
+    yourNameText.innerHTML = yourName;
+    heart.appendChild(p1);
+    let p2 = document.createElement("p");
+    crushNameText.innerHTML = crushName;
+    heart.appendChild(p2);
+    console.log(yourName);
+})
